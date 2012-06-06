bfbcca8914d16469ea41f3b1a93032ce9349d932
Mattias <mattiaspgames@gmail.com>
2012-06-06 06:42:18 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Island::Application.config.secret_token = '4cf5d72d22c03a5fd4722993e5b2655a4a24614471996061b751563bf8e54032a2aca8670f0bf3bfa6426b52ba01c42ca006650f699abc5b5acf52d8172f84ea'
